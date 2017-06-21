7a6a20078b6e58bf1d0e9afed983135ff8858b13
George Higbie <gehigbie@gmail.com>
2017-06-21 22:40:18 +0000

    Connect views to routes

--- modules/app.js
+++ modules/app.js
@@ -1,2 +1,11 @@
 /* global angular */
-angular.module('myApp', []);
+angular.module('myApp', ['ngRoute']).config(config);
+
+function config($routeProvider) {
+    $routeProvider.when('/', {
+        template: '<h1>This is the MAIN page.</h1>'
+    }).when('/about', {
+        template: '<h1>The is the ABOUT page.</h1>'
+    });
+}
+        
